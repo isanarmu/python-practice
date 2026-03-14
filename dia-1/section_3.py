195f25c7321ae6d62a56ca84925b41c7e13349e6
isanarmu <isanarmu@gmail.com>
2026-03-14 16:22:41 +0100

    Update section_3.py

--- dia-1/section_3.py
+++ dia-1/section_3.py
@@ -1,24 +1,24 @@
 text = "Hello i'm a string"
 result = text[4]
 
-print (result)
+print ("result 1:",result)
 
 text_2 = "hello i'm another string"
 result_2 = text[-4]
-print(result_2)
+print("result 2:",result_2)
 
 text_3 = "hello i'm the third string"
 
 result_3 = text_3[0:4]
-print(result_3)
+print("result 3:",result_3)
 
 text_4 = "hello i'm the fourth string"
 result_4 = text_4.index("h", 4, 15)
 
-print(result_4)
+print("result 4:",result_4)
 
-# para buscar de derecha a izquierda:
+# para buscar de derecha a izquierda: (te devuelve contando de izq a der la primera que enciuentra de derecha a izquierda)
 
 text_5 = "hello i'm the fifth string"
 result_5 = text_5.rindex("h")
-print(result_5)
+print("result 5:",result_5)
