b1b97c448f7c6a8105a6a846f934704a0f9a0de7
isanarmu <isanarmu@gmail.com>
2026-03-21 19:17:20 +0100

    last save bf new pc

---
+++ dia-1/text_sect3.py
@@ -0,0 +1,15 @@
+text = "Probando un texto Random"
+# print(text.upper())
+# print(text.lower())
+# print(text[2].upper())
+# texto = text.split()
+# texto = text.split("t")
+# texto = text.find("t")
+texto = text.replace("t", "y")
+texto = text.replace("P", "X").replace("Random", "Cualquiera")
+print(texto)
+
+# t = "juntar"
+# s = "palabras"
+# r = " ".join([t,s])
+# print(r)
