bbe83ae90942c98bae66e911d9da6c42a1aecda5
Irene <isanarmu@gmail.com>
2026-03-22 16:03:12 +0100

    some changes

--- dia-1/text_sect3.py
+++ dia-1/text_sect3.py
@@ -13,3 +13,7 @@ print(texto)
 # s = "palabras"
 # r = " ".join([t,s])
 # print(r)
+long_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+print(len(long_text))
+X = "Repetición"
+print(x * 15)
