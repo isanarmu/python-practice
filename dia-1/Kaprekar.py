dfc02778ac890ced96a635644db43e703fb4e31f
Irene <isanarmu@gmail.com>
2026-04-05 16:34:33 +0200

    Update Kaprekar.py

--- dia-1/Kaprekar.py
+++ dia-1/Kaprekar.py
@@ -17,14 +17,33 @@
 def es_kaprekar(n):
     if n == 1:
         return True
+    
     cuadrado = n ** 2
-    cuadrado = str(cuadrado)
-    if (len(cuadrado) == 1):
-        cuadrado = "0" + cuadrado
-    mitad = len(cuadrado) // 2
-    izquierda = cuadrado[:mitad]
-    derecha = cuadrado[mitad:]
+    cuadrado_str = str(cuadrado)
+    if (len(cuadrado_str) == 1):
+        cuadrado_str = "0" + cuadrado_str
+    mitad = len(cuadrado_str) // 2
+    izquierda = cuadrado_str[:mitad]
+    derecha = cuadrado_str[mitad:]
     suma = int(izquierda) + int(derecha)
     return suma == n
-print(es_kaprekar(81))
+print(es_kaprekar(9))
     
+def is_kaprekar(n):
+    if n == 1:
+        return True
+    # calculamos el cuadrado y lo convertimos a texto
+    squared = n ** 2
+    squared_str = str(squared)
+    if len(squared_str) == 1:
+        squared_str = "0" + squared_str
+    # Paso 3: Buscamos la mitad de la palabra
+    mitad = len(squared_str) // 2 
+    # Paso 4: Cortamos con tijeras el texto por la mitad
+    trozo_izquierdo = squared_str[:mitad]
+    trozo_derecho = squared_str[mitad:]
+    # Paso 5: Convertimos los trozos de nuevo a números y los sumamos
+    suma = int(trozo_izquierdo) + int(trozo_derecho)
+    # Paso 6: Comprobamos si la suma de los trozos da el número original
+    return suma == n
+print(is_kaprekar(9)) 
