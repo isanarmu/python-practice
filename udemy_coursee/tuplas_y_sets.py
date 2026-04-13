318359035f0c73c15aa65673a5fd4c95c3712d67
Irene <isanarmu@gmail.com>
2026-04-13 11:30:46 +0200

    name changes

---
+++ udemy_coursee/tuplas_y_sets.py
@@ -0,0 +1,14 @@
+# numero de veces que aparece algo
+mi_tupla = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+print(mi_tupla.count(2))
+
+# Extrae los elementos de la siguiente tupla en cuatro variables: a, b, c, d
+mi_tupla = (1, 2, 3, 4)
+
+a, b, c, d = mi_tupla
+print(mi_tupla)
+
+# SETS, solo admiten elementos unicos, en un set los elementos no se pueden repetir, si añades un elemento repetido python lo omiten
+# no pueden almacenar listas ni diccionarios
+mi_set = set([1, 2, 3, 4, 5,5])
+print(mi_set)
