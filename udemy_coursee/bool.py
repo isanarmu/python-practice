11ab19f8198cd3b024a98c9ed15438438597f030
Irene <isanarmu@gmail.com>
2026-04-13 16:47:02 +0200

    name changes

--- udemy_coursee/bool.py
+++ udemy_coursee/bool.py
@@ -6,3 +6,9 @@ control = 2 in lis
 control2 = 7 not in lis
 print(control)
 print(control2)
+
+texto = "hola que tal estas"
+textoo= texto.split()
+textoo.reverse()
+resultado = " ".join(textoo)
+print(resultado)
--- udemy_coursee/erjcicio_analis_text.py
+++ udemy_coursee/erjcicio_analis_text.py
@@ -1,4 +1,28 @@
-texto = "Decir cuántas veces aparece cada una de las 3 letras dentro del texto. Importante: convertir todo a minúsculas para que no importe si el usuario escribió mayúsculas o minúsculas."
-letras = "a, b, c"
-texto_min = texto.lower()
-print(texto.count(letras))
+text= input("Ingresa un texto: ")
+texto = text.lower()
+
+letra_1 = input("Escribe la primera letra: ").lower()
+letra_2 = input("Escribe la segunda letra: ").lower()
+letra_3 = input("Escribe la tercera letra: ").lower()
+
+texto_min_1 = texto.count(letra_1)
+texto_min_2 = texto.count(letra_2)
+texto_min_3 = texto.count(letra_3)
+
+contador_palabras = texto.split()
+
+primera = texto[0]
+ultima = texto[-1]
+aparece_python = "python" in texto.lower()
+
+contador_palabras.reverse()
+texto_invertido = " ".join(contador_palabras)
+
+
+print(f"la letra {letra_1} aparece: {texto_min_1} veces")
+print(f"la letra {letra_2} aparece: {texto_min_2} veces")
+print(f"la letra {letra_3} aparece: {texto_min_3} veces")
+print(f"Aparecen {len(contador_palabras)} palabras.")
+print(f"La primera letra es {primera} y la última es {ultima}")
+print(f"el texto al reves: {texto_invertido}")
+print(aparece_python)
