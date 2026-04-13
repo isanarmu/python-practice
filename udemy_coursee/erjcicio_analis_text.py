58ffbd21d4b77012b4b489480ae41fcc849ac346
Irene <isanarmu@gmail.com>
2026-04-13 15:40:45 +0200

    Update erjcicio_analis_text.py

---
+++ udemy_coursee/erjcicio_analis_text.py
@@ -0,0 +1,4 @@
+texto = "Decir cuántas veces aparece cada una de las 3 letras dentro del texto. Importante: convertir todo a minúsculas para que no importe si el usuario escribió mayúsculas o minúsculas."
+letras = "a, b, c"
+texto_min = texto.lower()
+print(texto.count(letras))
