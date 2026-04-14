c1afb86e7b2139bb954bac4041ac4260e142b030
Irene <isanarmu@gmail.com>
2026-04-14 13:33:14 +0200

    name change and new folders

--- udemy_coursee/if_else.py
+++ udemy_coursee/if_else.py
@@ -2,14 +2,31 @@
 if 10 > 99:
     print("10 es mayor que nueve")
 else:
-    print("no es amyor")
+    print("no es mayor")
 
 
 operacion = 5 * 2
 
-if operacion == 10:
-    print("el resultado es 10")
-elif operacion != 30:
-    print("el resultado no es 30")
+if operacion != 10:
+    print("el resultado no es 10")
+elif operacion == 30:
+    print("el resultado es 30")
 else:
     print("no se cual es el resultado")
+
+
+mi_edad = int(input("Escribe aqui tu edad: "))
+calificacion = int(input("¿Qué nota has tenido?: "))
+
+if mi_edad >= 100:
+    print("Eres una momia y encima puedes conducir")
+elif mi_edad >= 18:
+        print("Puedes conducir un coche en España")
+else:
+    print("No puedes conducir un coche en españa")
+if calificacion > 10 or calificacion < 0:
+    print("Has introducido mal la nota, tiene que ser un numero entre 0 y 10")
+elif calificacion >= 5:
+    print("Has aprobado")
+else:
+    print("Lo siento has suspendido")
---
+++ udemy_coursee/prueba.py
@@ -0,0 +1,12 @@
+edad = 15
+tiene_licencia = True
+if edad >= 18 and tiene_licencia == False:
+        print("No puedes conducir. Necesitas contar con una licencia")
+elif edad >= 18 and tiene_licencia == True:
+    print("Puedes conducir")
+    
+else:
+    print("No puedes conducir aún. Debes tener 18 años y contar con una licencia")
+    
+
+
