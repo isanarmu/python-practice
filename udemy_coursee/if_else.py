046ca7c9df80a81c1f717e5ccca20251855c2003
Irene <isanarmu@gmail.com>
2026-04-14 12:56:57 +0200

    Update if_else.py

---
+++ udemy_coursee/if_else.py
@@ -0,0 +1,15 @@
+# if elif elif else. Siempre dos puntso al final de cada uno.
+if 10 > 99:
+    print("10 es mayor que nueve")
+else:
+    print("no es amyor")
+
+
+operacion = 5 * 2
+
+if operacion == 10:
+    print("el resultado es 10")
+elif operacion != 30:
+    print("el resultado no es 30")
+else:
+    print("no se cual es el resultado")
