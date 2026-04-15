0baa9522117641b5e4ccab3c65f121d19d2bcf10
Irene <isanarmu@gmail.com>
2026-04-15 19:44:51 +0200

    Update loops.py

--- udemy_coursee/loops.py
+++ udemy_coursee/loops.py
@@ -1,4 +1,9 @@
 def hola(que):
     if que == 0:
         return("FUck")
+        return("FUck")
+        return("FUck")
+        return("FUck")
+        return("FUck")
+        return("FUck")
 print(hola(0))
