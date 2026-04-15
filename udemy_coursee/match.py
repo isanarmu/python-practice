7fceaad86a71302671472d4e3506dad0ce406ebd
Irene <isanarmu@gmail.com>
2026-04-15 19:43:29 +0200

    name change and new folders

--- udemy_coursee/match.py
+++ udemy_coursee/match.py
@@ -18,4 +18,32 @@ match serie:
         print("Mootorola")
     case _:
         print("No se encontro")
-    
+
+
+cliente = {"nombre": "Federico",
+           "edad": 50,
+           "ocupacion": "instructor"}
+
+pelicula = {"titulo": "Matrix",
+            "ficha_tecnica": {"protagonista": "Keanu Reeves",
+                              "director": "Lana y Lily Wachowski"}}
+
+libro = {"titulo": "1984",
+         "autor": "George Orwell"}
+
+elementos = [cliente, pelicula, libro]
+
+for e in elementos:
+    match e:
+        case {"nombre": nombre,
+              "edad": edad,
+              "ocupacion": ocupacion}:
+            print("este es un cliente")
+            print(nombre, edad, ocupacion)
+        case {"titulo": titulo,
+              "ficha_tecnica": {"protagonista": protagonista,
+                                "director": director}}:
+            print("este es una pelicula")
+            print(titulo, protagonista, director)
+        case _:
+            print("unknown")
