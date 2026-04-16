b23d6c4e6117f6c977e5cc3976046b43b6e1fae9
Irene <isanarmu@gmail.com>
2026-04-16 16:35:18 +0200

    Update loops.py

--- udemy_coursee/loops.py
+++ udemy_coursee/loops.py
@@ -3,3 +3,22 @@ mi_lista = ["a", "b", "c"]
 for letra in mi_lista:
     numeros_letra = mi_lista.index(letra)
     print(f"la letra es {letra} y su indice es {numeros_letra}")
+
+
+# otra_lista = ["Pablo", "Pepe", "lola", "Brunno"]
+
+# for nombre in otra_lista:
+#     if nombre.startswith("B"):
+#         print(nombre)
+#     else:
+#         print("este nombre no empieza con b")
+
+
+otra_lista = ["Lola, Pepe, Brunno"]
+
+for nombre in otra_lista:
+    if nombre.startswith("B"):
+        print(nombre)
+    else:
+        print("este nombre no empieza con b")
+
