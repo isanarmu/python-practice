91efe0f6f67abb6667107091c8e89b125e31042f
Irene <isanarmu@gmail.com>
2026-04-16 16:33:39 +0200

    Update loops.py

--- udemy_coursee/loops.py
+++ udemy_coursee/loops.py
@@ -2,4 +2,4 @@ mi_lista = ["a", "b", "c"]
 
 for letra in mi_lista:
     numeros_letra = mi_lista.index(letra)
-    print(f"la letra es {letra} y su indice{numeros_letra}")
+    print(f"la letra es {letra} y su indice es {numeros_letra}")
