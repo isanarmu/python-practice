0c1a020acbe3b7ab1b1ffa9f288d807a6acb86b1
Irene <isanarmu@gmail.com>
2026-04-17 19:33:34 +0200

    Update loop_while.py

--- udemy_coursee/loop_while.py
+++ udemy_coursee/loop_while.py
@@ -55,7 +55,7 @@ print("-------------------------------------interrumpir flujo cuando llegue a nu
 lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
 
 for numero in lista_numeros:
-    print(numero)
     if numero < 0:
         break
+    print(numero)
     
