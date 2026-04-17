6adff523455df525f5560d7f1a784a30467080c1
Irene <isanarmu@gmail.com>
2026-04-17 19:21:44 +0200

    Update loop_while.py

--- udemy_coursee/loop_while.py
+++ udemy_coursee/loop_while.py
@@ -48,3 +48,12 @@ while numero >= 0:
     if numero % 5 == 0:
         print(numero)
     numero = numero - 1
+
+print("------------------------------------------------------------------------------------")
+# loopinterrumpir flujo cuando llegue a un valor negativo
+
+lista_numeros = [4,5,8,7,6,9,8,2,4,5,7,1,9,5,6,-1,-5,6,-6,-4,-3]
+
+for numero in lista_numeros:
+    if numero < 0:
+        break
