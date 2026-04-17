aa16a3e0a9fc1a9fbde0bf3362887e51e06b7dad
Irene <isanarmu@gmail.com>
2026-04-17 19:52:01 +0200

    Update range.py

--- udemy_coursee/range.py
+++ udemy_coursee/range.py
@@ -14,3 +14,12 @@ print("---------------------")
 lista = list(range(1, 11))
 
 print(lista)
+
+print(".--------------------------.")
+
+suma_cuadrados = 0
+for n in range(1, 16):
+    
+    suma_cuadrados = n ** 2
+    n = suma_cuadrados
+    print(n)
