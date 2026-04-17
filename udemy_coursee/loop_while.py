04fee920e16aed4cf9590995e47ff1fb3766b706
Irene <isanarmu@gmail.com>
2026-04-17 18:17:46 +0200

    Update loop_while.py

--- udemy_coursee/loop_while.py
+++ udemy_coursee/loop_while.py
@@ -1,7 +1,17 @@
+print("------------------------------------------------------------------------------------")
 monedas = 5
-
 while monedas > 0:
     print(f"tengo {monedas} monedas")
     monedas -= 1
 else:
-    print("no tengo ams monedas")
+    print("no tengo mas monedas")
+
+print("------------------------------------------------------------------------------------")
+respuesta = "s"
+
+while respuesta == "s":
+    respuesta = input("¿quieres continuar? (s/n): ")
+else:
+    print("Adios")
+
+print("------------------------------------------------------------------------------------")
