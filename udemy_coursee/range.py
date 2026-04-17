58c0b141940c318844a5f980b76dbb9cb7ba26dd
Irene <isanarmu@gmail.com>
2026-04-17 19:44:35 +0200

    Update range.py

---
+++ udemy_coursee/range.py
@@ -0,0 +1,16 @@
+
+for n in range(0, 10):
+    print(n)
+print("---------------------")
+for n in range(5):
+    print(n)
+
+print("---------------------")
+
+for n in range(0, 10,2):
+    print(n)
+
+print("---------------------")
+lista = list(range(1, 11))
+
+print(lista)
