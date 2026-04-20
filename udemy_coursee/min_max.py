9ead1fb2dfdfbdc6cc5483a06777b1754108350f
Irene <isanarmu@gmail.com>
2026-04-20 13:39:17 +0200

    trying

--- udemy_coursee/min_max.py
+++ udemy_coursee/min_max.py
@@ -3,3 +3,8 @@ print(menor)
 
 lista = [58, 58, 54, 152, 12]
 print(f'el menor es: {min(lista)}, y el mayor es: {max(lista)}.')
+
+print("-----------------------------------------------strings----------------------------")
+
+nombres = ['Rachel', 'Marcos', 'Luis', 'Rea']
+print(min(nombres))
