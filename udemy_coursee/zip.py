85b61c825d073124e2c2162200ea8b61c9bfcb46
Irene <isanarmu@gmail.com>
2026-04-20 10:53:33 +0200

    trying

--- udemy_coursee/zip.py
+++ udemy_coursee/zip.py
@@ -1,5 +1,12 @@
 nombres = ['Jesus', 'Celia', 'Eva', 'irene']
-edades = [65, 65, 36, 26]
+edades = [65, 60, 36, 26]
+ciudades = ['Armuña', 'Armuña', 'Armuña', 'Armuña']
 
-combinados = list(zip(nombres, edades))
-print(combinados)
+combinados = list(zip(nombres, edades, ciudades))
+
+for nombre, edad, ciudad in combinados:
+    if nombre == 'Celia':
+        print(f'{nombre} tenia {edad} años y vivia en {ciudad}')
+    else:
+        print(f'{nombre} tiene {edad} años y vive en {ciudad} ')
+
