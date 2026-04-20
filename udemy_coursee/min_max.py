9238a896a723b99770edcf9dd2c30a3fc2398230
Irene <isanarmu@gmail.com>
2026-04-20 13:47:49 +0200

    Update min_max.py

--- udemy_coursee/min_max.py
+++ udemy_coursee/min_max.py
@@ -12,3 +12,10 @@ print(min(nombres))
 dic = {"c1": 45, "c2": 11}
 
 print(min(dic.values()))
+
+print("---------------------ejercicio udemy----------------------------------")
+diccionario_edades = {"Carlos":55, "María":42, "Mabel":78, "José":44, "Lucas":24, "Rocío":35, "Sebastián":19, "Catalina":2,"Darío":49}
+
+edad_minima = min(diccionario_edades.values())
+ultimo_nombre = max(diccionario_edades.keys())
+print(edad_minima, ultimo_nombre)
