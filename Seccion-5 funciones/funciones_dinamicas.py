8b231ddce0bf02dc868efe05d22e1c29f0074701
Irene <isanarmu@gmail.com>
2026-04-24 19:52:00 +0200

    trying

--- Seccion-5 funciones/funciones_dinamicas.py
+++ Seccion-5 funciones/funciones_dinamicas.py
@@ -1,5 +1,11 @@
-def chequear_3_cifras(numero):
-    return numero in range(100, 1000)
-resultado = chequear_3_cifras(658)
+print("-----------------------------------------------------------------")
 
+def chequear_3_cifras(n1, n2):
+    suma = n1 + n2
+    return suma in range(100, 1000)
+
+resultado = chequear_3_cifras(25, 25)
 print(resultado)
+
+print("-----------------------------------------------------------------")
+
