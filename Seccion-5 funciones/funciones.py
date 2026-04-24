3957e051b6e60534ae7573001742f41260103844
Irene <isanarmu@gmail.com>
2026-04-24 19:05:39 +0200

    trying

--- Seccion-5 funciones/funciones.py
+++ Seccion-5 funciones/funciones.py
@@ -1,12 +1,26 @@
+print("---------------------------------------------------------")
+
 def saludar(nombre):
     """
     estafuncion sirve para saludar
     """
     print("Hola " + nombre)
-
 saludar("Brunno")
 saludar("THor")
 
-def saludar():
-    print("Hola mundo!")
-saludar()
+print("---------------------------------------------------------")
+
+def bienvenida(nombre_persona):
+    print(f"¡Bienvenido {nombre_persona}!" )
+nombre_persona = "Laura"
+bienvenida(nombre_persona)
+
+print("---------------------------------------------------------")
+
+
+def cuadrado(numero):
+    return numero ** 2
+
+print(cuadrado(2))
+
+print("---------------------------------------------------------")
