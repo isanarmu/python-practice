151f649207b99e235552f0c4c82ac99c76a36c14
Irene <isanarmu@gmail.com>
2026-04-24 19:48:47 +0200

    trying

--- Seccion-5 funciones/funciones.py
+++ Seccion-5 funciones/funciones.py
@@ -24,3 +24,26 @@ def cuadrado(numero):
 print(cuadrado(2))
 
 print("---------------------------------------------------------")
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+reusltado = multiplicar(5, 10)
+
+print(reusltado)
+
+print("---------------------------------------------------------")
+
+def usd_a_eur(valor_usd):
+    return valor_usd * 0.90
+
+dolares = 4
+print(usd_a_eur(dolares))
+
+print("---------------------------------------------------------")
+
+def invertir_palabra(palabra):
+    return palabra[::-1].upper()
+word = "Brunno"
+
+print(invertir_palabra(word))
