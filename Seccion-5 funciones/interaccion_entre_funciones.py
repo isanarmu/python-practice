8d5edb3867988831a2ed3b30c8b731e2035505b1
Irene <isanarmu@gmail.com>
2026-04-26 12:13:49 +0200

    trying

---
+++ Seccion-5 funciones/interaccion_entre_funciones.py
@@ -0,0 +1,18 @@
+lista_cafes = [("capuchino", 2.30), ("latte", 2.80), ("espresso", 1.50), ("mocha", 3.20), ("americano", 2.00), ("flat white", 3.00), ("macchiato", 2.40)]
+
+for elemento, precio in lista_cafes:
+    print(elemento)
+
+print("------------------------probando---------------------")
+
+def encontrar_mas_caro(lista):
+    precio_mayor = 0
+    nombre_cafe_mas_caro = ""
+
+    for nombre, precio in lista:
+        if precio > precio_mayor:
+            precio_mayor = precio
+            nombre_cafe_mas_caro = nombre
+    
+    return (f"el cafe mas caro es {nombre_cafe_mas_caro} y su precio {precio_mayor}€")
+print(encontrar_mas_caro(lista_cafes))
