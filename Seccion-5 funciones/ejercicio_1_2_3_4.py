3fa06887d1ed2336831184772b20a415250bf9fb
Irene <isanarmu@gmail.com>
2026-04-30 14:51:02 +0200

    trying

--- Seccion-5 funciones/ejercicio_1_2_3_4.py
+++ Seccion-5 funciones/ejercicio_1_2_3_4.py
@@ -16,6 +16,18 @@ print(devolver_distintos(1, 2, 3 ))
 print("-------------------.ordenar letras-----------------")
 
 def ordenar(string):
-     letras = list(string)
+     letras = set(string)
      return sorted(letras)
 print(ordenar("entretenido"))
+
+print("-------------------0 repetido-----------------")
+
+def repetido(*args):
+     
+     for i in range(len(args)- 1): 
+        
+        if args[i] == 0 and args[i + 1] == 0:
+             return True
+     return False
+print(repetido(5,6,1,0,9,3,5))
+    
