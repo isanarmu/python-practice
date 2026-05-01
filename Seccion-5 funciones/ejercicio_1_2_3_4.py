da84e60a593f5efe6f55648ab77854856bad1e92
Irene <isanarmu@gmail.com>
2026-05-01 11:58:13 +0200

    trying

--- Seccion-5 funciones/ejercicio_1_2_3_4.py
+++ Seccion-5 funciones/ejercicio_1_2_3_4.py
@@ -30,4 +30,27 @@ def repetido(*args):
              return True
      return False
 print(repetido(5,6,1,0,9,3,5))
+
+
+print("-------------------contar primos-----------------")
+
+def contar_primos(numero):
+    primos = [2]
+    interacion = 3
+    if numero < 2:
+         return 0
+    while interacion <= numero:
+         for n in range(3, interacion, 2):
+              if interacion % n == 0:
+                   interacion += 2
+                   break
+         else:
+              primos.append(interacion)
+              interacion += 2
+    print(primos)
+    return len(primos)
+print(contar_primos(50))
+
+    
+              
     
