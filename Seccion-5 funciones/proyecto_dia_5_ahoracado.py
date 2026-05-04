04a55fe7e6bacde31ab1549e6c5ef360deb21b20
Irene <isanarmu@gmail.com>
2026-05-04 12:48:53 +0200

    Update proyecto_dia_5_ahoracado.py

---
+++ Seccion-5 funciones/proyecto_dia_5_ahoracado.py
@@ -0,0 +1,9 @@
+
+
+
+palabras = ["abundante", "brunno", "felicidad", "prosperidad", "riqueza"]
+palabra_oculta =["-","-","-","-"]
+vidas = 6
+
+pedir_letra = input("Elige una letra")
+print(pedir_letra)
