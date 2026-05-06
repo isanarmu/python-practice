7d1120a08d4a0f670b4543b6e1168d30417df1fa
Irene <isanarmu@gmail.com>
2026-05-06 12:51:42 +0200

    trying

--- seccion-6/manipulacion_archivos.py
+++ seccion-6/manipulacion_archivos.py
@@ -1,2 +1,10 @@
 mi_archivo = open("prueba.txt")
-print(mi_archivo)
+
+primera_linea = mi_archivo.readline()
+print(primera_linea.upper())
+
+siguiente_linea = mi_archivo.readline()
+print(siguiente_linea)
+
+# siempre que se abra un archivo recuerda cerrarlo para que no consuma mucha memoria
+mi_archivo.close()
