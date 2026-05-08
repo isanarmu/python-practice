b4673b5fe3fdac4db86241744a304cda92df1a14
Irene <isanarmu@gmail.com>
2026-05-08 10:51:43 +0200

    trying

---
+++ seccion-6/os.py
@@ -0,0 +1,11 @@
+import os
+
+# ruta = os.getcwd()
+# print(ruta)
+
+os.chdir("C:\\Users\\Irene\\Desktop\\Probando rutas")
+
+archivo = open("vesina.txt.txt")
+print(archivo.read())
+
+archivo.close()
