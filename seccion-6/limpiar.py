1b1cfe9318a9de976691ef6193c357b20f28e827
Irene <isanarmu@gmail.com>
2026-05-11 20:55:24 +0200

    trying

---
+++ seccion-6/limpiar.py
@@ -0,0 +1,7 @@
+from os import system
+
+
+nombre = input("dime tu nombre: ")
+edad = input("dime tu edad: ")
+system("cls")
+print(f"Tu nombre es {nombre} y tu edad {edad}")
--- seccion-6/modificar_archivos.py
+++ seccion-6/modificar_archivos.py
@@ -1,4 +1,4 @@
-mi_archivo = open("prueba1.txt", "w")
+mi_archivo = open("prueba1.txt", "w") #metdo "a" R", a es para caudnoe scribe un archivo comenzar a escribir solo al final de lo que ya tenia hecho
 
 mi_archivo.write("Nueva linea")
 
@@ -6,7 +6,24 @@ mi_archivo.write("Nueva linea")
 
 
 
+archivo = open("prueba1.txt", "a")
+
+archivo.write("Nuevo inicio de sesión")
+archivo = open("prueba1.txt", "r")
+print(archivo.read())
+
+archivo.close()
 
 
 
 mi_archivo.close()
+
+registro_ultima_sesion = ["Federico", "\t20/12/2021", "\t08:17:32 hs", "\tSin errores de carga"]
+
+archivo = open("registro.txt", "a")
+archivo.writelines(registro_ultima_sesion)
+archivo = open("registro.txt", "r")
+
+print(archivo.read())
+
+archivo.close()
