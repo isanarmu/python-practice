89e6be73598c7c361cd9c661459158a411b0acfd
Irene <isanarmu@gmail.com>
2026-05-11 11:39:03 +0200

    trying

--- seccion-6/path.py
+++ seccion-6/path.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 # abrir obejo path con metodos distinots | no hace falta open oclose
 carpeta = Path("C:\\Users\\Irene\\Desktop\\Probando rutas\\vesina.txt.txt")
+base = Path.home()
+print(f"esto es la base: ", base)
 
 print(carpeta.name)
 print(carpeta.suffix)
