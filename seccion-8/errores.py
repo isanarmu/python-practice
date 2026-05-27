71b290c07da8bd871f2db6948c96068b1e947516
Irene <isanarmu@gmail.com>
2026-05-27 10:39:05 +0200

    trying

--- seccion-8/errores.py
+++ seccion-8/errores.py
@@ -1,6 +1,6 @@
 def al_caudrado():
     numero = int(input("ingresa un numero: "))
-    resultado = numero ** numero
+    resultado = numero * numero
     print(f"el cuadrado de {numero} es {resultado}")
 
 
@@ -10,3 +10,10 @@ try:
 except:
     # el codigo que ddebe ejhecutarse si falla
     print("ocurrio un error imprevisto")
+
+else: 
+    # El codigo que se ejecutará si no sale error
+    print("El cuadrado se ejecutó correctamente")
+finally:
+    # el código que se ejecutará de todos modos
+    print("Gracias por usar nuestro programa")
