7cef2fa4ab686d9224a9438303f78199698a9505
Irene <isanarmu@gmail.com>
2026-06-01 13:06:34 +0200

    trying

---
+++ seccion-8/decoradores.py
@@ -0,0 +1,41 @@
+def cambiar_letras(tipo):
+
+    def mayuscula(texto):
+        print(texto.upper())
+
+    def minuscula(texto):
+        print(texto.lower())
+
+    if tipo == "may":
+        return mayuscula
+    elif tipo == "min":
+        return minuscula
+
+
+operacion = cambiar_letras("min")
+
+operacion("Palabra")
+
+
+def decorar_saludo(funcion):
+
+    def otra_funcion(palabra):
+        print("Hola")
+        funcion(palabra)
+        print("Adiós")
+
+    return otra_funcion
+
+
+@decorar_saludo
+def mayuscula(texto):
+    print(texto.upper())
+
+
+@decorar_saludo
+def minuscula(texto):
+    print(texto.lower())
+
+
+mayuscula("Python")
+
--- seccion-8/probando_errores.py
+++ seccion-8/probando_errores.py
@@ -3,3 +3,7 @@ def imprimir():
     """imprime algo"""
     numero1 = 500
     print(numero1)
+
+# lo que tengo que poner en la terminl apra ver erroes       
+# C:\Users\Irene\Documents\GitHub\python-practice\seccion-8>python -m pylint probando_errores.py -ry
+#  python -m pylint probando_errores.py -ry
