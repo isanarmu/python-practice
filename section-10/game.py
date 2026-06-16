13f9a06b41b3155285367662d1a3ea34d7761fc0
Irene <isanarmu@gmail.com>
2026-06-16 11:07:17 +0200

    practicing python

--- section-10/game.py
+++ section-10/game.py
@@ -5,3 +5,14 @@ pygame.init()
 
 # crear la pantalla
 pantalla = pygame.display.set_mode((800, 600))
+
+# loop juego para cerrar y que se qeude abierto
+
+se_ejecuta = True
+while se_ejecuta:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            se_ejecuta = False
+        
+    pantalla.fill((230, 220, 240))
+    pygame.display.update()
