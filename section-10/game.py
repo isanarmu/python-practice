7048334ebc9184ec8d157edfc10901c3c15be2fd
Irene <isanarmu@gmail.com>
2026-06-16 11:31:14 +0200

    practicing python

--- section-10/game.py
+++ section-10/game.py
@@ -12,8 +12,16 @@ icono = pygame.image.load("pizza.png")
 pygame.display.set_icon(icono)
 # para el fondo
 fondo = pygame.image.load("fondo.png")
-fondo = pygame.transform.scale(fondo, (800, 900))
+fondo = pygame.transform.scale(fondo, (800, 600))
 
+# repartirdor
+repartidor_img = pygame.image.load("repartidor.png")
+repartidor_img = pygame.transform.scale(repartidor_img, (64, 100))
+repartidor_x = 368
+repartidor_y = 440
+
+def repartidor(x, y):
+    pantalla.blit(repartidor_img, (x, y))
 
 # loop juego para cerrar y que se qeude abierto
 
@@ -24,6 +32,7 @@ while se_ejecuta:
             se_ejecuta = False
         
     pantalla.blit(fondo, (0, 0))
+    repartidor(repartidor_x, repartidor_y)
     pygame.display.update()
     
 pygame.quit()
