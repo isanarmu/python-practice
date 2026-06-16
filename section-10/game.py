6d29f6b3bb6e3cee97b8042b06fc3d7030373e41
Irene <isanarmu@gmail.com>
2026-06-16 11:21:47 +0200

    practicing python

--- section-10/game.py
+++ section-10/game.py
@@ -5,6 +5,15 @@ pygame.init()
 
 # crear la pantalla
 pantalla = pygame.display.set_mode((800, 600))
+# para el nombre que aparece al abrirse
+pygame.display.set_caption("Pizza survivor")
+# para el icono del juego
+icono = pygame.image.load("pizza.png")
+pygame.display.set_icon(icono)
+# para el fondo
+fondo = pygame.image.load("fondo.png")
+fondo = pygame.transform.scale(fondo, (800, 900))
+
 
 # loop juego para cerrar y que se qeude abierto
 
@@ -14,7 +23,7 @@ while se_ejecuta:
         if evento.type == pygame.QUIT:
             se_ejecuta = False
         
-    pantalla.fill((230, 220, 240))
+    pantalla.blit(fondo, (0, 0))
     pygame.display.update()
     
 pygame.quit()
