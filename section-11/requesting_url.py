60830112dd402cbecaeda7f2264cec19584f4215
Irene <isanarmu@gmail.com>
2026-08-30 14:35:03 +0200

    s
    descarga de imagen desdeapgina web

--- section-11/requesting_url.py
+++ section-11/requesting_url.py
@@ -4,10 +4,40 @@ import requests
 resultado = requests.get('https://irenes-portfolio.vercel.app/about')
 sopa = BeautifulSoup(resultado.text, 'lxml')
 
-# print(sopa.select('title'))
+print(sopa.select('title'))
 # para contarlos usaria len(sopa.....)
 # para el indice  al final despues de ('h2')[0]
 
 # para buscar con id necesto poenr hastagh
 
-print(len(sopa.select('#Introduction')))
+# print(sopa.select('#Introduction'))
+
+for titulo in sopa.select('title'):
+    print(titulo.get_text())
+
+# para descargar la imagen
+
+imagen = sopa.select('link[as="image"]')[0]
+
+url_base = "https://irenes-portfolio.vercel.app"
+ruta_imagen = imagen.get("href")
+
+print(url_base + ruta_imagen)
+
+imagenes = sopa.select('img')
+print(imagenes)
+
+# para descargarlo
+
+imagen = sopa.select('link[as="image"]')[0]
+ruta_imagen = imagen.get("href")
+
+url_imagen = url_base + ruta_imagen
+
+respuesta_imagen = requests.get(url_imagen)
+
+archivo = open("imagen_descargada.png", "wb")
+archivo.write(respuesta_imagen.content)
+archivo.close()
+
+print("Imagen descargada correctamente")
