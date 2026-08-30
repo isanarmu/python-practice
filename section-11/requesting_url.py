7e29352057d77162a1169ac42f4920ec18b46a4b
Irene <isanarmu@gmail.com>
2026-08-30 13:19:03 +0200

    Update requesting_url.py

--- section-11/requesting_url.py
+++ section-11/requesting_url.py
@@ -4,4 +4,10 @@ import requests
 resultado = requests.get('https://irenes-portfolio.vercel.app/about')
 sopa = BeautifulSoup(resultado.text, 'lxml')
 
-print(sopa.select(('title')))
+# print(sopa.select('title'))
+# para contarlos usaria len(sopa.....)
+# para el indice  al final despues de ('h2')[0]
+
+# para buscar con id necesto poenr hastagh
+
+print(len(sopa.select('#Introduction')))
