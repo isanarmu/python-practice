3fba16e081c38a068f5d4a96d2e1ef11d010bc57
Irene <isanarmu@gmail.com>
2026-09-08 12:58:40 +0200

    s
    s

--- section-11/Proyecto_web_scraping.py
+++ section-11/Proyecto_web_scraping.py
@@ -3,3 +3,7 @@ import requests
 
 resultado = requests.get('https://books.toscrape.com/')
 sopa = BeautifulSoup(resultado.text, 'lxml')
+
+for numero_pagina in range(1, 51):
+    url = f'https://books.toscrape.com/catalogue/page-{numero_pagina}.html'
+    print(url)
