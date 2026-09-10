d3b15b85ce6bea6faaaf2e5bb198dcc457de8c03
Irene <isanarmu@gmail.com>
2026-09-10 17:28:45 +0200

    Update app.py

--- section-12/app.py
+++ section-12/app.py
@@ -7,11 +7,35 @@ st.caption("Version 1.0")
 
 categorias = ["Food", "Transportation", "Essentials", "Leisure", "Home", "Health", "Other"]
 
-descripcion = st.text_input("Description of the expense or income", placeholder="Write the description of the operation")
-cantidad = st.number_input("Amount", step=1.0, min_value=0.0, format="%0.2f")
-fecha = st.date_input("Date")
-categoria = st.selectbox("Category", categorias)
-tipo = st.radio("Type", ["Income", "Expense"], horizontal=True)
+if "transacciones" not in st.session_state:
+    st.session_state.transacciones = []
+
+with st.form("New transaction"):
+    descripcion = st.text_input("Description of the expense or income", placeholder="Write the description of the operation")
+    cantidad = st.number_input("Amount", step=1.0, min_value=0.0, format="%0.2f")
+    fecha = st.date_input("Date")
+    categoria = st.selectbox("Category", categorias)
+    tipo = st.radio("Type", ["Income", "Expense"], horizontal=True)
+    enviado = st.form_submit_button("Submit")
+
+if enviado:
+    st.session_state.transacciones.append({"Description": descripcion,
+                                            "amount": cantidad,
+                                            "date": fecha,
+                                            "category": categoria,
+                                            "type": tipo,
+                                            "send": enviado})
+    st.success("Transaction added succesfully")
+st.write("Acumulated transactions: ")
+for transaccion in st.session_state.transacciones:
+    st.write(transaccion)
 
 st.write("Summary: ")
-variable = st.write(f"{tipo} of ${cantidad} in {categoria} - {descripcion} the {fecha} ")
+st.write(f"{tipo} of ${cantidad} in {categoria} - {descripcion} the {fecha} ")
+
+if "contador" not in st.session_state:
+    st.session_state.contador = 0
+
+if st.button("Incrementar"):
+    st.session_state.contador += 1
+    st.write("contador : ", st.session_state.contador)
