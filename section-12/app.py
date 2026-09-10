d7fa4bfffac05eadbb55850f6dac6e80c7a5ca17
Irene <isanarmu@gmail.com>
2026-09-10 13:24:41 +0200

    Update app.py

--- section-12/app.py
+++ section-12/app.py
@@ -5,8 +5,9 @@ st.title("Personal Finance Tracker")
 st.write("Track all your expenses and earnings simple and visual")
 st.caption("Version 1.0")
 
-categorias = ["Comidas", "Transporte", "Necesidades", "Ocio", "Hogar", "Salud", "Otros"]
+categorias = ["Food", "Transportation", "Essentials", "Leisure", "Home", "Health", "Other"]
 
 descripcion = st.text_input("Description of the expense or income", placeholder="Write the description of the operation")
 cantidad = st.number_input("Amount", step=1.0, min_value=0.0, format="%0.2f")
 fecha = st.date_input("Date")
+categoria = st.selectbox("Category", categorias)
